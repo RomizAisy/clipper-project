e7c6e56a7c58f722581f940f8b2484ecd3aee9a0
Romiz Aisy <romizaisy21@gmail.com>
2026-02-20 22:23:14 +0700

    my own checkpoint, its melting my brain

--- queue_config.py
+++
@@ -1,4 +0,0 @@
-from rq import Queue
-from redis_conn import redis_conn
-
-clipper_queue = Queue("clipper", connection=redis_conn)
--- redis_conn.py
+++
@@ -1,3 +0,0 @@
-from redis import Redis
-
-redis_conn = Redis(host="localhost", port=6379, db=0)
